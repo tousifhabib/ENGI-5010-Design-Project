2ed76fdb754f654a4d7a927d0f75a6fe66d59109
Tousif Habib <tousif.habib@e-bydesign.ca>
2021-02-06 19:39:33 -0330

    E5P-2 Made Platform

--- GameEngine.py
+++ GameEngine.py
@@ -2,20 +2,73 @@ import pygame
 from pygame.locals import *
 
 pygame.init()
+
 screen_width = 500
 screen_height = 500
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Platformer')
 
-#Load Images
+# define game variables
+tile_size = 50
+# load images
+background_img = pygame.image.load('img/background_img.png')
 
+def draw_grid():
+    for line in range(0, 11):
+        pygame.draw.line(screen, (255, 255, 255), (0, line * tile_size), (screen_width, line * tile_size))
+        pygame.draw.line(screen, (255, 255, 255), (line * tile_size, 0), (line * tile_size, screen_height))
+
+class World():
+    def __init__(self, data):
+        self.tile_list = []
+        # load images
+        dirt_img = pygame.image.load('img/Grasstop.png')
+
+        row_count = 0
+        for row in data:
+            col_count = 0
+            for tile in row:
+                if tile == 1:
+                    img = pygame.transform.scale(dirt_img, (tile_size, tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+                col_count += 1
+            row_count += 1
+
+    def draw(self):
+        for tile in self.tile_list:
+            screen.blit(tile[0], tile[1])
+
+
+world_data = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
+
+world = World(world_data)
 
 run = True
-while run == True:
 
+while run:
+    screen.blit(background_img, (-500, 0))
+    #draw_grid()
+    world.draw()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-    
+
+    pygame.display.update()
+
 pygame.quit()
