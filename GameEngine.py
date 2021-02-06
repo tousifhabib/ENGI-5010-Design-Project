1bf7441083374c377ef05ddcec2f5e372cf2c850
Tousif Habib <tousif.habib@e-bydesign.ca>
2021-02-06 16:19:44 -0330

    E5P-2 Updated code

--- GameEngine.py
+++ GameEngine.py
@@ -1,1 +1,21 @@
-#GAMEENGINE1
+import pygame
+from pygame.locals import *
+
+pygame.init()
+screen_width = 500
+screen_height = 500
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Platformer')
+
+#Load Images
+
+
+run = True
+while run == True:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    
+pygame.quit()
