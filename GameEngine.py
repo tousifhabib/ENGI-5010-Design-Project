e4ef38e534468b059aee57815e491a1560b11b97
Tousif Habib <tousif.habib@e-bydesign.ca>
2021-02-06 20:45:45 -0330

    E5P-2 Changed code for game enging
    Using different method for platformer

--- GameEngine.py
+++ GameEngine.py
@@ -1,74 +1,62 @@
-import pygame
-from pygame.locals import *
+import pygame as pg
+import random
+from Settings import *
 
-pygame.init()
 
-screen_width = 500
-screen_height = 500
+class Game:
+    def __init__(self):
+        # Initialize game window
+        pg.init()
+        pg.mixer.init()
+        self.screen = pg.display.set_mode((screen_width, screen_height))
+        pg.display.set_caption(TITLE)
+        self.clock = pg.time.Clock()
+        self.running = True
 
-screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption('Platformer')
+    def newGame(self):
+        # Starts new game
+        self.all_sprites = pg.sprite.Group()
 
-# define game variables
-tile_size = 50
-# load images
-background_img = pygame.image.load('img/background_img.png')
+    def consoleRunner(self):
+        # Game loop
+        self.playing = True
+        while self.playing:
+            self.clock.tick(FPS)
+            self.events()
+            self.update()
+            self.draw()
 
-def draw_grid():
-    for line in range(0, 11):
-        pygame.draw.line(screen, (255, 255, 255), (0, line * tile_size), (screen_width, line * tile_size))
-        pygame.draw.line(screen, (255, 255, 255), (line * tile_size, 0), (line * tile_size, screen_height))
+    def update(self):
+        # Game loop update
+        self.all_sprites.update()
 
-class World():
-    def __init__(self, data):
-        self.tile_list = []
-        # load images
-        dirt_img = pygame.image.load('img/Grasstop.png')
-
-        row_count = 0
-        for row in data:
-            col_count = 0
-            for tile in row:
-                if tile == 1:
-                    img = pygame.transform.scale(dirt_img, (tile_size, tile_size))
-                    img_rect = img.get_rect()
-                    img_rect.x = col_count * tile_size
-                    img_rect.y = row_count * tile_size
-                    tile = (img, img_rect)
-                    self.tile_list.append(tile)
-                col_count += 1
-            row_count += 1
+    def events(self):
+        # Game loop events
+        for event in pg.event.get():
+            # Check for closing window
+            if event.type == pg.QUIT():
+                if self.playing:
+                    self.playing = False
+                self.running = False
 
     def draw(self):
-        for tile in self.tile_list:
-            screen.blit(tile[0], tile[1])
+        # Game loop draw
+        self.screen.fill(YELLOW)
+        self.all_sprites.draw(self.screen)
+        # After drawing flip display
+        pg.display.flip()
+
+    def show_start_screen(self):
+        pass
+
+    def show_game_over_screen(self):
+        pass
 
 
-world_data = [
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-]
+game = Game()
+game.show_start_screen()
+while game.running:
+    game.newGame()
+    game.show_game_over_screen()
 
-world = World(world_data)
-
-run = True
-
-while run:
-    screen.blit(background_img, (-500, 0))
-    #draw_grid()
-    world.draw()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-
-    pygame.display.update()
-
-pygame.quit()
+pg.quit()
